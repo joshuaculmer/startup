2a9023f8204aff4edfc664ed2e869755d5212865
Joshua Culmer <163606650+joshuaculmer@users.noreply.github.com>
2024-11-09 10:18:41 -0700

    Fixed(title), Added(Header, Footer) to app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -7,8 +7,44 @@ function App() {
 
     return (
         <div>
-            Test app running. On page app jsx
-        </div>);
+            <link rel="icon" type="image/x-icon" href="favicon-32x32.png"></link>
+            <header class="sticky-top">
+                <nav class="navbar navbar-expand-sm bg-light navbar-light">
+                    <div class="container-fluid">
+                        <ul class="navbar-nav nav-tabs me-auto mb-2 mb-lg-0">
+                            <a class="nav-item nav-link" href="index.html">Home</a>
+                            <a class="nav-item nav-link" href="dancemoves.html">Dance Moves</a>
+                            <a class="nav-item nav-link" href="myMoves.html">MyDance Moves</a>
+                            <a class="nav-item nav-link" href="comboCalculator.html">Combo Calculator</a>
+                            <a class="nav-item nav-link" href="share.html">Share</a>
+                            <a class="nav-item nav-link" href="about.html">About</a>
+                            <a class="nav-item nav-link active" href="login.html">Login</a>
+                            <li class="nav-item">
+                                <span class="navbar-text">UserName Placeholder</span>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+
+            <main>App components go here</main>
+
+            <footer>
+                <nav className="navbar bg-light">
+                    <div className="container-fluid justify-content-center text-muted">
+                        <div className="nav-item px-2">Joshua Culmer</div>
+                        <div className="nav-item px-2">
+                            <a href="https://github.com/joshuaculmer/startup">https://github.com/joshuaculmer/startup</a>
+                        </div>
+                    </div>
+                </nav>
+            </footer>
+
+
+
+
+        </div >
+    );
 }
 
 
