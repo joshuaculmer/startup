2aab34882280b118f2920eca6c88803386b860ec
Joshua Culmer <163606650+joshuaculmer@users.noreply.github.com>
2024-11-15 14:29:50 -0700

    Integrated Service Login

--- src/login/unathenticated.jsx
+++ src/login/unathenticated.jsx
@@ -8,13 +8,28 @@ export function Unauthenticated(props) {
     const [displayError, setDisplayError] = React.useState(null);
 
     async function loginUser() {
-        localStorage.setItem('userName', userName);
-        props.onLogin(userName);
+        loginOrCreate(`/api/auth/login`);
     }
 
     async function createUser() {
-        localStorage.setItem('userName', userName);
-        props.onLogin(userName);
+        loginOrCreate(`/api/auth/create`);
+    }
+
+    async function loginOrCreate(endpoint) {
+        const response = await fetch(endpoint, {
+            method: 'post',
+            body: JSON.stringify({ email: userName, password: password }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        });
+        if (response?.status === 200) {
+            localStorage.setItem('userName', userName);
+            props.onLogin(userName);
+        } else {
+            const body = await response.json();
+            setDisplayError(`⚠ Error: ${body.msg}`);
+        }
     }
 
     return (
@@ -31,9 +46,9 @@ export function Unauthenticated(props) {
                 <Button variant='primary' onClick={() => loginUser()} disabled={!userName || !password}>
                     Login
                 </Button>
-                {/* <Button variant='secondary' onClick={() => createUser()} disabled={!userName || !password}>
+                <Button variant='secondary' onClick={() => createUser()} disabled={!userName || !password}>
                     Create
-                </Button> */}
+                </Button>
             </div>
 
             <MessageDialog message={displayError} onHide={() => setDisplayError(null)} />
