23030406381da555b26fd44634e222b1c181efff
Joshua Culmer <163606650+joshuaculmer@users.noreply.github.com>
2024-11-23 17:38:20 -0700

    Used canvas fix on database.js to sync client vers

--- service/database.js
+++ service/database.js
@@ -4,10 +4,9 @@ const uuid = require('uuid');
 const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
-const client = new MongoClient(url);
-const db = client.db('simon');
+const client = new MongoClient(url, { tls: true, serverSelectionTimeoutMS: 3000, autoSelectFamily: false, });
+const db = client.db('dancing');
 const userCollection = db.collection('user');
-const scoreCollection = db.collection('score');
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
