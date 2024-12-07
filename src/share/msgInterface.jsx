57b8585382f6626418245f339db57cd763a98e43
Joshua Culmer <163606650+joshuaculmer@users.noreply.github.com>
2024-12-07 12:02:15 -0700

    Msg now can be sent with enter key

--- src/share/msgInterface.jsx
+++ src/share/msgInterface.jsx
@@ -6,11 +6,29 @@ import { MsgEvent, MsgNotifier } from './msgNotifier';
 export function MsgInterface(props) {
 
     const [msgText, setMsgText] = React.useState('')
+
+    function sendMsg() {
+        console.log(msgText)
+        setMsgText('')
+    }
+
     return (
         <div>
             <div className='input-group mb-3'>
                 {/* <span className='input-group-text'>@</span> */}
-                <input className='form-control' type='text' value={msgText} onChange={(e) => setMsgText(e.target.value)} placeholder='Your message here' />
+                <input className='form-control' type='text' value={msgText}
+                    onChange={(e) => setMsgText(e.target.value)}
+                    placeholder='Your message here'
+                    onKeyUp={(e) => {
+                        console.log(e)
+                        if (e.key === 'Enter') {
+                            sendMsg()
+                        }
+                    }
+                    } />
+                <Button variant='primary' onClick={() => sendMsg()}>
+                    Send
+                </Button>
             </div>
 
             Hi {props.userName}, you'll be able to send messages here
