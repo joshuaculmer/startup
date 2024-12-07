155beaae4663abc676b053480dcdcc37a5beec8a
Joshua Culmer <163606650+joshuaculmer@users.noreply.github.com>
2024-12-07 11:38:55 -0700

    msgInterface now accepts input and displays it

--- src/share/msgInterface.jsx
+++ src/share/msgInterface.jsx
@@ -4,10 +4,18 @@ import { Button } from 'react-bootstrap';
 import { MsgEvent, MsgNotifier } from './msgNotifier';
 
 export function MsgInterface(props) {
+
+    const [msgText, setMsgText] = React.useState('')
     return (
         <div>
+            <div className='input-group mb-3'>
+                {/* <span className='input-group-text'>@</span> */}
+                <input className='form-control' type='text' value={msgText} onChange={(e) => setMsgText(e.target.value)} placeholder='Your message here' />
+            </div>
 
             Hi {props.userName}, you'll be able to send messages here
+
+            Test message: {msgText}
 
         </div>
 
